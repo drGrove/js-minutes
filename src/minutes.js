c93c309b984d0c60bbce22f8362b068cfdd30d8a
Danny Grove <danny@drgrovellc.com>
2014-04-08 00:36:40 -0400

    Adds .jshintrc and cleans up file to pass jshint

--- src/minutes.js
+++ src/minutes.js
@@ -1,3 +1,4 @@
+'use strict';
 /**
  * Adds minutes to a time provided
  * @param {string} time - String time [H}H:MM A
@@ -9,28 +10,30 @@ function addMinutes(time,minutes){
     time = time.split(' ')
     period = time[1]
     newTime = time[0].split(':')
-    if(period == "PM")
+    if(period === "PM"){
         newTime[0] = Number(newTime[0]) + 12
+    }
     
     var timeAsMinutes = (Number(newTime[0]) * 60) + Number(newTime[1]) + minutes;
     var timeAsDecimal = Number(timeAsMinutes) / 60;
     var reconvertedTime = timeAsDecimal.toString().split('.');
     var hours = Number(reconvertedTime[0]);
-    var minutes = Number("." + reconvertedTime[1]) * 60;
-    var finalTime = [hours, Math.round(minutes)];
+    var mins = Number("." + reconvertedTime[1]) * 60;
+    var finalTime = [hours, Math.round(mins)];
     if(Number(finalTime[1]) > 60){
         finalTime[0] = Number(finalTime[0]) + (Number(finalTime[1]) / 60)
         finalTime[1] = (Number(finalTime[0]) % 60) + Number(finalTime[1])
     }
     if(Number(finalTime[0]) >= 12){
         finalTime[0] = Number(finalTime[0]) - 12
-        period = period == "PM" ? "AM" : "PM"
+        period = period === "PM" ? "AM" : "PM"
     }else{
         finalTime[0] = parseInt(Number(finalTime[0]), 10)
     }
-    if(Number(finalTime[0]) == 0){
+    if(Number(finalTime[0]) === 0){
         finalTime[0] = 12
         period = "PM"
     }
     return finalTime.join(":") + " " + period
 }
+
